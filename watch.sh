8290018c0f1213ddd3b9cc0af487794ab9e650d9
Nathann Cohen <nathann.cohen@gmail.com>
2016-07-20 18:24:43 +0200

    Parallelism improvement

--- watch.sh
+++ watch.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+block_size=10
+function batchfind () {
+    find "$*" -type f | tr '\n' ' ' | perl -ape "s/(([^ ]* ){0,$block_size})/\1\t/g" | tr '\t' '\n' | grep -E "."
+}
+
 # Recomputes 'all_parsed' whenever parse_entry gets modified
 while [ 1 ]; do
     find parse_entry -newer all_parsed | grep "" > /dev/null || inotifywait -e MODIFY parse_entry
     date
-    for i in arxiv/4-bibs/*; do
-	echo "cat \"$i\" | ./parse_entry";
+    echo "Parsing begins"
+    batchfind "arxiv/4-bibs/" | while read i; do
+	echo "cat $i | ./parse_entry";
     done | parallel > all_parsed
 done
