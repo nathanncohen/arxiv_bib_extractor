d469130336663ee6e6976389bdf72c0f6fea9493
Nathann Cohen <nathann.cohen@gmail.com>
2016-08-03 17:49:52 +0200

    Simplification du word matching

--- load_parsed.py
+++ load_parsed.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 import re
+import codecs
 def get_tag(s,x):
     st=s.index(x+'=')+len(x)+1
     return s[st:s.index('╣',st)]
 
-def load_parsed():
-    return [dict(re.findall("╠([^=╣]*)=*([^╣]*)",x))
-            for x in open("../all_parsed",'r').readlines()]
+def load_parsed(shattered=False):
+    entries = codecs.open("../all_parsed",'r',encoding='utf8').readlines()
+    if shattered:
+        regex = re.compile(ur"^([ ,0-9[[:punct:]]]*╠[^╣]*╣[ ,0-9[[:punct:]]]*)*$")
+        entries = [x for x in entries if re.match(regex,x)]
+
+    return [dict(re.findall(ur"╠([^=╣]*)=*([^╣]*)╣",x))
+            for x in entries]
